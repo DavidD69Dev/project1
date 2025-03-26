5a746d9dd54d8fea6f20329035c7f96e6fd40d89
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-03-26 16:19:20 +0100

    fixed the carousel and added some vignettes

--- index.js
+++ index.js
@@ -92,81 +92,18 @@ resetButton.addEventListener("click", function () {
 // // Initialisation des filtres au chargement de la page
 applyFilters();
 
-// /* CARTE VERSION MOBILE - en cours de travail...
-//                     // L'id du container, par exemple <div id="map"></div>
-//                     var mapID = 'map';
-
-//                     // Plan IGN avec une transparence de 80%
-//                     var PlanIGN = L.tileLayer('https://data.geopf.fr/wmts?'+
-//                         '&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&TILEMATRIXSET=PM'+
-//                         '&LAYER={ignLayer}&STYLE={style}&FORMAT={format}'+
-//                         '&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}',
-//                         {
-//                             ignLayer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2',
-//                             style: 'normal',
-//                             format: 'image/png',
-//                             service: 'WMTS',
-//                             opacity: 0.8,
-//                             attribution: 'Carte © IGN/Geoplateforme'
-//                     });
-
-//                     // Photographies aériennes en-dessous de Plan IGN
-//                     let OrthoIGN = L.tileLayer('https://data.geopf.fr/wmts?'+
-//                         '&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&TILEMATRIXSET=PM'+
-//                         '&LAYER={ignLayer}&STYLE={style}&FORMAT={format}'+
-//                         '&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}',
-//                         {
-//                             ignLayer: 'ORTHOIMAGERY.ORTHOPHOTOS',
-//                             style: 'normal',
-//                             format: 'image/jpeg',
-//                             service: 'WMTS'
-//                     });
-//                     // Ma carte
-//                     let myMap = L.map(mapID, {
-//                         center: [45.75036994086184, 4.828724700166397],
-//                         zoom: 25,
-//                         layers: [OrthoIGN,PlanIGN]
-//                     })
-// */
-
-// /*const thumbnail=document.querySelector(".thumbnail")
-// const thumbnailPopup=document.querySelector(".thumbnail-popup")
-
-// thumbnail.addEventListener("click", () => {
-//     const screenWidth = window.innerWidth;
-//     if(screenWidth<=800){
-//         if(thumbnail.style.display==="flex"){
-//             thumbnail.style.display="none";
-//             thumbnailPopup.style.display="flex"
-//     }else if(screenWidth>800){
-//         if(thumbnail.style.display==="flex")
-//             thumbnailPopup.style.display="none";
-//         }
-//     else{thumbnail.style.display="flex"}
-//         }
-//     }
-// )
-
-// const cancel=document.querySelector("#cancel")
-
-// cancel.addEventListener("click", () =>{
-//     if (thumbnailPopup.style.display==="flex"){
-//         thumbnailPopup.style.display="none";
-//         thumbnail.style.display="flex"
-//     }else{thumbnail.style.display="flex"}
-//     }
-// )*/
-//  main
-
 /* carousel  */
-
-const buttons = document.querySelectorAll(".btn");
-const slides = document.querySelectorAll(".slide");
+/* Carousels.forEach permet de boucler sur chaque carousel individuellement ! 
+Attention à cibler les buttons/slides l'interieur des carousels (carousel.querySelectorAll)*/
+const carousels = document.querySelectorAll(".carousel")
+carousels.forEach(carousel => {
+const buttons = carousel.querySelectorAll(".btn");
+const slides = carousel.querySelectorAll(".slide");
 
 buttons.forEach((button) => {
 	button.addEventListener("click", (btn) => {
 		const calculNextSlide = btn.target.id === "next" ? 1 : -1;
-		const slideActive = document.querySelector(".active");
+		const slideActive = carousel.querySelector(".active");
 
 		newIndex = [...slides].indexOf(slideActive) + calculNextSlide;
 
@@ -177,4 +114,4 @@ buttons.forEach((button) => {
 
 		slideActive.classList.remove("active");
 	});
-});
+})});
